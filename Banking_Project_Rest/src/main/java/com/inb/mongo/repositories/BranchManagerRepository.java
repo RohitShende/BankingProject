2e2de3e04ba487ecbe38dc6c19fa6f9805674bcb
palakhjude <palakhjude@gmail.com>
2015-12-07 15:14:32 +0530

    Space added by Palakh

--- Banking_Project_Rest/src/main/java/com/inb/mongo/repositories/BranchManagerRepository.java
+++ Banking_Project_Rest/src/main/java/com/inb/mongo/repositories/BranchManagerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.inb.mongo.collections.BranchManager;
 
 public interface BranchManagerRepository extends MongoRepository<BranchManager, String>{
-
+ 
 }
