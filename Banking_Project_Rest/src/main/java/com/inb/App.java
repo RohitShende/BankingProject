08e6dbf8d97e7560851bbea6fd339c5c788dc036
navinmaheshwari305 <navin.maheshwari305@gmail.com>
2015-12-08 16:27:59 +0530

    Updated App.java

--- Banking_Project_Rest/src/main/java/com/inb/App.java
+++ Banking_Project_Rest/src/main/java/com/inb/App.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.inb.util.AdminUtil;
+
 /**
  * Hello world!
  *
@@ -12,12 +14,11 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
-public class App 
-{
-	
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! I am Shariful" ); 
-        SpringApplication.run(App.class, args);
-    }
+public class App {
+
+	public static void main(String[] args) throws Exception {
+		AdminUtil.populateAdmin();
+		System.out.println("populated...");
+		SpringApplication.run(App.class, args);
+	}
 }
