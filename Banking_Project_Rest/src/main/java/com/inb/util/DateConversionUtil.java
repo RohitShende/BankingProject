750c4544f0a5f2beb9e00071dbb7b3ba9cd13336
RohitShende <rohitshende16@gmail.com>
2015-12-09 21:15:16 +0530

    changed access of calendar.YEAR to static Calendar.YEAR

--- Banking_Project_Rest/src/main/java/com/inb/util/DateConversionUtil.java
+++ Banking_Project_Rest/src/main/java/com/inb/util/DateConversionUtil.java
@@ -12,7 +12,7 @@ public class DateConversionUtil {
 	{
 		Calendar calendar=Calendar.getInstance();
 		calendar.setTime(branchManager.getDateOfBirth());
-		Calendar calendar2=new GregorianCalendar(calendar.get(calendar.YEAR),calendar.get(calendar.MONTH),calendar.get(calendar.DAY_OF_MONTH)+1);
+		Calendar calendar2=new GregorianCalendar(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)+1);
 		Date isoDate=calendar2.getTime();
 		return isoDate;
 	}
