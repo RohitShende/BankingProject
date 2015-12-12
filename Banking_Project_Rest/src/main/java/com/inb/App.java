d4a2aab5c765709018e3a047b139df3c2de31fcb
RohitShende <rohitshende16@gmail.com>
2015-12-12 20:13:27 +0530

    App.java imports organized

--- Banking_Project_Rest/src/main/java/com/inb/App.java
+++ Banking_Project_Rest/src/main/java/com/inb/App.java
@@ -1,18 +1,10 @@
 package com.inb;
 
-import java.util.Date;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.inb.mongo.repositories.UnregisteredCustomerRepository;
-import com.inb.rest.controllers.UnregisteredCustomerController;
-import com.inb.rest.entity.Account;
-import com.inb.rest.entity.AccountType;
-import com.inb.rest.entity.UnregisteredCustomerPOJO;
-import com.inb.service.interfaces.UnregisteredCustomerService;
 import com.inb.util.AdminUtil;
 
 /**
@@ -25,10 +17,7 @@ import com.inb.util.AdminUtil;
 public class App {
 
 	public static void main(String[] args) throws Exception {
-		// AdminUtil.populateAdmin();
+		AdminUtil.populateAdmin();
 		SpringApplication.run(App.class, args);
-
-		
-
 	}
 }
