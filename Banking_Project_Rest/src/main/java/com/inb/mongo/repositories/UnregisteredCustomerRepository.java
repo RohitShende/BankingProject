e5e4e328cc58fa55cd22f48ac2762a9c500a2b30
RohitShende <rohitshende16@gmail.com>
2015-12-14 16:28:41 +0530

    organised imports on UnregisteredCustomerRepository

--- Banking_Project_Rest/src/main/java/com/inb/mongo/repositories/UnregisteredCustomerRepository.java
+++ Banking_Project_Rest/src/main/java/com/inb/mongo/repositories/UnregisteredCustomerRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-import com.inb.mongo.collections.BranchManager;
 import com.inb.mongo.collections.Person;
 import com.inb.mongo.collections.UnregisteredCustomer;
 import com.inb.rest.entity.Account;
