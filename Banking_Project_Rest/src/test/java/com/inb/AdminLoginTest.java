a49b7f704132b1e747754faa8ea71333d45b4e50
RohitShende <rohitshende16@gmail.com>
2015-12-14 17:33:28 +0530

    AdminLoginTest is working now

--- Banking_Project_Rest/src/test/java/com/inb/AdminLoginTest.java
+++ Banking_Project_Rest/src/test/java/com/inb/AdminLoginTest.java
@@ -1,10 +1,12 @@
 package com.inb;
 
-import org.junit.Assert;
-import org.junit.Before;
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 public class AdminLoginTest {
@@ -13,7 +15,7 @@ public class AdminLoginTest {
 	static Client client;
 	static WebResource target;
 	
-	@Before
+	@BeforeClass
 	public static void init(){
 		client = Client.create();
 		target = client.resource("http://localhost:8080/admin/login");
@@ -21,20 +23,17 @@ public class AdminLoginTest {
 
 	@Test
 	public void testLoginAdminSuccess() {
-		String input = "{\"username\":\"nm\",\"password\":\"nm\"}";
-		String result = target.accept("application/json")
-				.type("application/json").post(String.class, input);
-		String expected = "{\"id\":\"5666bd07f1bdf2fa152fde6e\",\"firstName\":\"Navin\",\"lastName\":\"mahesh\",\"email\":\"nm@gmail.com\",\"phone\":123,\"address\":\"MYaddress\",\"dateOfBirth\":1417977000000,\"username\":\"admin\",\"password\":\"password123\"}";
-		Assert.assertEquals(expected, result);
+		String input = "{\"userName\":\"admin\",\"password\":\"password123\"}";
+		ClientResponse response=target.accept("application/json").type("application/json").post(ClientResponse.class,input);
+		assertEquals(200,response.getStatus());
 	}
 
 	@Test
 	public void testLoginAdminFailure() {
 		String input = "{\"username\":\"nm\",\"password\":\"nmm\"}";
-		String result = target.accept("application/json")
-				.type("application/json").post(String.class, input);
-		String expected = "{ \"error\" :100}";
-		Assert.assertEquals(expected, result);
+		String result = target.accept("application/json").type("application/json").post(String.class, input);
+		String expected = "{ \"error\" :100,\"message\": \"Invalid Credentials\"}";
+		assertEquals(expected, result);
 	}
 
 }
