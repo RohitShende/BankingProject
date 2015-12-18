78c8588560cbb9fa3a8c8ddc204cf91e3c1dbe2c
sharifulislam21 <sharifulislammallik@gmail.com>
2015-12-18 11:44:53 +0530

    Removed unnecessary codes from mongo.collections

--- Banking_Project_Rest/src/main/java/com/inb/mongo/collections/Customer.java
+++ Banking_Project_Rest/src/main/java/com/inb/mongo/collections/Customer.java
@@ -8,12 +8,10 @@ public abstract class Customer extends Person {
 	public Customer(String firstName, String lastName, String email,
 			long phone, String address, Date dateOfBirth) {
 		super(firstName, lastName, email, phone, address, dateOfBirth);
-		// TODO Auto-generated constructor stub
 	}
 
 	public Customer() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 }
